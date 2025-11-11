44078b7f2ec0fe60f178b541e706f532907c8d53
Bảo Toàn <103725538+baotoanproject@users.noreply.github.com>
2025-11-11 10:57:32 +0700

    Update auto-rotate-monitor.sh

--- auto-rotate-monitor.sh
+++ auto-rotate-monitor.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
 OUTPUT="HDMI-1"
 ROTATE_SCRIPT="/usr/local/bin/rotate-screen.sh"
 
 while true; do
-    # Kiểm tra HDMI có kết nối không
+    bash /usr/local/bin/reload-hdmi.sh
     CONNECTED=$(xrandr | grep "$OUTPUT connected")
     if [ -n "$CONNECTED" ]; then
-        # HDMI đang kết nối => gọi script xoay
         bash "$ROTATE_SCRIPT"
     fi
     sleep 5
